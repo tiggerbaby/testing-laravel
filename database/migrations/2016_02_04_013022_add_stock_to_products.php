29e210ac6d01ead901e217324b10826b20fa9b70
Shaoting Wu <alice.wu.nz@gmail.com>
2016-02-04 15:05:20 +1300

    add migrate to products table

---
+++ database/migrations/2016_02_04_013022_add_stock_to_products.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddStockToProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->smallInteger('stock');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('stock');
+        });
+    }
+}
